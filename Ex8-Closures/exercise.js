337abb15137db66d6eb677e9070004e2724074fc
mete <metehandoenmez@gmail.com>
2023-04-09 17:24:21 +0300

    changed and completed exercise 8

--- Ex8-Closures/exercise.js
+++ Ex8-Closures/exercise.js
@@ -1,3 +1,10 @@
 function multiplyByTwo(value) {
   let number = 2;
+  function inner () {
+    let result = number * value;
+    return result;
+  }
+  return inner;
 }
+
+console.log(multiplyByTwo(4)());
