807cd06fc031eb75a8e63d612e60497f8a6103de
mete <metehandoenmez@gmail.com>
2023-04-10 01:23:20 +0300

    changed and completed exercise 10

--- Ex10-Optional-Chaining/exercise-2.js
+++ Ex10-Optional-Chaining/exercise-2.js
@@ -1,5 +1,21 @@
-const order = {};
+const order = {
+  customer: {
+    address: {
+      city: ""
+    }
+  }
+};
+
+/* made everything except the city value truthy*/
 
 if (order && order.customer && order.customer.address && !order.customer.address.city) {
   console.log('City is required');
 }
+
+//simplified code:
+
+if(!order?.customer?.address?.city) {
+  console.log("City is required");
+}
+
+//prints City is required (falseness of city value is true, empty string -> false)
