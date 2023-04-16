c42f103facba011c6c67534e9cd638187a4e33ff
mete <metehandoenmez@gmail.com>
2023-04-17 01:19:03 +0300

    changed and completed exercise 18

--- Exs18-Static-Method/exercise-3.js
+++ Exs18-Static-Method/exercise-3.js
@@ -1,7 +1,12 @@
 class Person {
+  firstName;
+  lastName;
   constructor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
+  }
+  static fromObject(object) {
+    return new Person(object.firstName, object.lastName);
   }
 }
 
@@ -12,3 +17,10 @@ const obj = {
 
 const person = Person.fromObject(obj);
 console.log(person.firstName + " " + person.lastName);
+
+
+const obj2 = {
+  firstName: "Mike"
+}
+const person2 = Person.fromObject(obj2);
+console.log(person2.firstName + " " + person.lastName);
