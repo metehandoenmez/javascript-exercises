03b59d5d0a9f76617568575a639853c4d8141cae
mete <metehandoenmez@gmail.com>
2023-04-17 06:18:08 +0300

    changed and completed exercise 19

--- Exs19-Getters-and-Setters/exercise-7.js
+++ Exs19-Getters-and-Setters/exercise-7.js
@@ -1,5 +1,35 @@
 class Person {
-  // ...
+  #firstName;
+  #lastName;
+  #age;
+
+  constructor(firstName, lastName, age) {
+    this.#firstName = firstName;
+    this.#lastName = lastName;
+    this.#age = age;
+  }
+
+  set firstName(val) {
+    this.#firstName = val;
+  }
+  set lastName(val) {
+    this.#lastName = val;
+  }
+  set age(val) {
+    this.#age = val;
+  }
+  get firstName() {
+    return this.#firstName;
+  }
+  get lastName() {
+    return this.#lastName;
+  }
+  get age() {
+    return this.#age;
+  }
+  get fullName() {
+    return this.#firstName + " " + this.lastName;
+  }
 }
 
 const person = new Person('Mario', 'Rossi', 25);
