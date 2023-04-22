ab9222b836bd4154a24cb8c7a942442f4ea54b60
mete <metehandoenmez@gmail.com>
2023-04-22 19:02:34 +0300

    changed and completed exercise 22

--- Exs22-Destructuring/exercise-2.js
+++ Exs22-Destructuring/exercise-2.js
@@ -4,10 +4,6 @@ const person = {
   lastName: 'Rossi',
   age: 25
 };
-
-const id = person.id;
-const firstName = person.firstName;
-const lastName = person.lastName;
-const age = person.age;
+let {id, firstName, lastName, age} = person
 
 console.log(id, firstName, lastName, age);
