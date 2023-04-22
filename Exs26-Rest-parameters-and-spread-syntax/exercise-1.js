42079ed4862748fdec69440dce4277ebe7c309f8
mete <metehandoenmez@gmail.com>
2023-04-23 00:43:50 +0300

    changed and completed exercise 26

--- Exs26-Rest-parameters-and-spread-syntax/exercise-1.js
+++ Exs26-Rest-parameters-and-spread-syntax/exercise-1.js
@@ -1,5 +1,8 @@
-function sum(num1, num2, num3, num4, num5) {
+/* function sum(num1, num2, num3, num4, num5) {
     return num1 + num2 + num3 + num4 + num5;
-}
+} */
 
+function sum(...numbers) {
+    return numbers.reduce((a,b) => a + b);
+}
 console.log(sum(1, 2, 3, 4, 5));
