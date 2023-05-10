2f547923ca7011a9e97320526faf48b6cf4c07c0
mete <metehandoenmez@gmail.com>
2023-05-11 00:16:35 +0300

    changed and completed exercise 34

--- Exs34-Callbacks/exercise.js
+++ Exs34-Callbacks/exercise.js
@@ -1,1 +1,14 @@
+function printAsync(callback, name) {
+setTimeout(() => {
+    callback();
+}, 1000);
+setTimeout(() => {
+    console.log(name)
+}, 2000);
+}
 
+function cbfunc1() {
+    console.log("Hello");
+}
+
+printAsync(cbfunc1, "Mete")
