58d5dd53f7bb5afde480d70101bbf98f4e20918e
mete <metehandoenmez@gmail.com>
2023-05-11 00:43:47 +0300

    changed and completed exercise 35

--- Exs35-Callbacks/exercise.js
+++ Exs35-Callbacks/exercise.js
@@ -1,1 +1,28 @@
+function repeatHello(callback) {
+    setInterval(callback , 1000);
+    }
+    
+    let cbfunc = () => {
+        console.log("Hello");
+    }
+    
+    repeatHello(cbfunc);
 
+
+
+/* function repeatHello(callback) {
+setInterval(() => {
+    callback(); 
+}, 1000);
+}
+
+function cbfunc() {
+    console.log("Hello");
+}
+
+repeatHello(cbfunc); */
+
+//setInterval takes an arrow function as its argument in this case to ensure that
+//the this value within the function remains the same even if the function is executed
+//in a different context like setInterval. Arrow functions inherit the this value from
+//the parent scope where they were defined, which can help avoid potential issues with this binding.
