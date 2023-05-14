71e50226f6f32bbd9d3159ba3f86f7be629bb37a
mete <metehandoenmez@gmail.com>
2023-05-15 01:04:07 +0300

    changed and completed exercise 44

--- Exs44-Local-Storage/exercise.js
+++ Exs44-Local-Storage/exercise.js
@@ -3,3 +3,11 @@ const user = {
   name: "John",
   age: 25,
 };
+
+function loadLocal() {
+  let jsonObj = localStorage.getItem("user");
+  let obj = JSON.parse(jsonObj);
+  console.log(obj);
+}
+
+loadLocal();
