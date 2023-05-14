48bfa18b69ac785c752697fcfbfa94467b4c060b
mete <metehandoenmez@gmail.com>
2023-05-14 15:39:25 +0300

    changed and completed exercise 37

--- Exs37-Promise/exercise.js
+++ Exs37-Promise/exercise.js
@@ -1,1 +1,14 @@
 const number = 15;
+
+let greater = new Promise((resolve, reject) => {
+    if (number > 10) {
+        resolve(number);
+    }
+    else {
+        reject(new Error())
+    }
+})
+
+greater
+.then((result) => {console.log(result);})
+.catch((error) => {console.error(error);})
