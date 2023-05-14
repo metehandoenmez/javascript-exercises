cf08f3395a9de95fe27ef12db150aee5970e68ee
mete <metehandoenmez@gmail.com>
2023-05-14 15:16:51 +0300

    changed and completed exercise 36

--- Exs36-Callbacks/exercise.js
+++ Exs36-Callbacks/exercise.js
@@ -1,1 +1,12 @@
-
+function repeatHello(callback) {
+    let id = setInterval(callback , 1000);
+    setTimeout(() => {
+        clearInterval(id);
+    }, 5000);
+    }
+    
+    let cbfunc = () => {
+        console.log("Hello");
+    }
+    
+    repeatHello(cbfunc);
