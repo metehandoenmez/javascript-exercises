0cc5e4f5f923e87a452ad692241d559c708ae94a
mete <metehandoenmez@gmail.com>
2023-05-14 15:27:39 +0300

    changed and completed exercise 37

--- Exs37-Promise/exercise.js
+++ Exs37-Promise/exercise.js
@@ -1,1 +1,14 @@
 const number = 15;
+
+let greater10 = new Promise((resolve, reject) => {
+   if (number > 10) {
+    resolve("Number is greater than 10!");
+   } 
+   else {
+    reject("Number is less than or equal to 10.");
+   }
+});
+
+greater10
+.then((result) => {console.log(result);})
+.catch((error) => {console.error(error);})
