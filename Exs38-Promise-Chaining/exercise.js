0b551f5d0e7ec07c2fcd3475b6c03061893a42f4
mete <metehandoenmez@gmail.com>
2023-05-14 16:20:32 +0300

    changed and completed exercise 38

--- Exs38-Promise-Chaining/exercise.js
+++ Exs38-Promise-Chaining/exercise.js
@@ -1,1 +1,35 @@
 const isLogged = true;
+
+let promise1 = (isLogged) => {
+    return new Promise((resolve, reject) => {
+        if (isLogged) {
+            let random = Math.random();
+            resolve(random)
+        }
+        else {
+            reject(new Error("Logged in = false"));
+        }
+})
+};
+
+let promise2 = (number) => {
+    return new Promise((resolve, reject) => {
+        if(number > 0.5) {
+            resolve({name: "John",age:24})
+        }
+        else {
+            reject(new Error())
+        }
+    })
+}
+
+promise1(isLogged)
+.then((random) => {
+return promise2(random);
+})
+.then((result) => {
+    console.log(result);
+})
+.catch((error) => {
+    console.error(error);
+})
